e2db8fd4aa2d504ccf6de4bb66a15e13eaa868a2
AbdulrhmanM0hamed <bodamohmed60@gmail.com>
2026-04-18 16:44:13 +0200

    add on signal notifaction

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ import 'package:zadana_user_v3/core/general_cubit/local_cubit.dart';
 import 'package:zadana_user_v3/core/l10n/translations/app_localizations.dart';
 import 'package:zadana_user_v3/core/services/language_service.dart';
 
+import 'package:zadana_user_v3/core/services/push_notification_service.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
+  PushNotificationService.init();
   runApp(DevicePreview(builder: (context) => const MyApp()));
 }
 
